4d1c9b39070f6e186d48ddaa6dcb258bbbc13c83
Muhammad Junaid <junaidirshad098@gmail.com>
2021-10-24 04:09:29 +0500

    Initial commit

---
+++ app/src/main/java/com/example/fb_keygenerate/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.fb_keygenerate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.security.MessageDigest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        try {
+            PackageInfo info =     getPackageManager().getPackageInfo("com.example.fb_keygenerate",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String sign= Base64.encodeToString(md.digest(), Base64.DEFAULT);
+                Log.d("MY KEY HASH:", sign);
+                Toast.makeText(getApplicationContext(),sign,     Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
